e2eab566c941009254b31b8dcfee63acdd34ebe9
“Miki” <masumotomiki18@gmail.com>
2020-07-05 06:48:20 +0000

    WIP

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-    def setup
+  def setup
     @user = users(:michael)
+    @other_user = users(:archer)
+  end
+    # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
   test "should get new" do
@@ -11,15 +22,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should redirect edit when not logged in" do
-    get edit_user_path(@user) ##!!!!!!TODO
+    get edit_user_path(@user) 
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name, ##!!!!!!TODO
+    patch user_path(@user), params: { user: { name: @user.name, 
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+   test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
 end
